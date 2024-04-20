1d8b2d1ccadfc5760a6651449e447eeab5f124aa
st <ststxx00@gmail.com>
2024-04-20 19:16:15 +0900

    auth basic

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), CatModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    CatModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/constants.ts
@@ -0,0 +1,5 @@
+export const jwtConstants = {
+  // TODO: Change secret value, store this value in env.
+  secret:
+    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+};
