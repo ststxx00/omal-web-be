6c72bacf1a7effb166e8cab4b857ef82a57ddef5
st <ststxx00@gmail.com>
2024-04-20 16:17:28 +0900

    cat CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatModule } from './cat/cat.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), CatModule],
   controllers: [AppController],
   providers: [AppService],
 })
