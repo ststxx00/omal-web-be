8b039a7b4dee9b355b7018ba9f12c7bbc82d4d5b
st <ststxx00@gmail.com>
2024-04-20 20:15:47 +0900

    unused lines

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,9 +6,7 @@ import {
   HttpStatus,
   Post,
   Request,
-  UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import { Public } from './decorator/public.decorator';
 
@@ -23,7 +21,6 @@ export class AuthController {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
-  //   @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
