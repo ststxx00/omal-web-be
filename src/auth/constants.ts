0661bff0d76b96076f81506190eccaebe5ebd5c8
st <ststxx00@gmail.com>
2024-04-20 23:33:22 +0900

    secret key

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,5 +1,3 @@
 export const jwtConstants = {
-  // TODO: Change secret value, store this value in env.
-  secret:
-    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+  secret: 'jhstomalsecret',
 };
