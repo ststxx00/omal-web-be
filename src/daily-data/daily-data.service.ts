3e20182642707729da3dbf9e67d26723f4502165
ST <ststxx00@gmail.com>
2024-12-18 01:42:40 +0900

    독서, 복음 스크래핑

--- src/daily-data/daily-data.service.ts
+++ src/daily-data/daily-data.service.ts
@@ -4,6 +4,7 @@ import { DailyData } from './entities/daily-data.entity';
 import { Model } from 'mongoose';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { Reading } from 'src/types';
 
 @Injectable()
 export class DailyDataService {
@@ -36,7 +37,7 @@ export class DailyDataService {
     }
   }
 
-  async scrapScriptures($: cheerio.CheerioAPI, index: number): Promise<ScrapRes> {
+  async scrapScriptures($: cheerio.CheerioAPI, index: number): Promise<Reading> {
     let titleIndex = -1;
     $('.bd_tit').each((i, el) => {
       if ($(el).text().includes(`제${index}독서`)) {
@@ -45,35 +46,74 @@ export class DailyDataService {
       }
     });
     const title = titleIndex !== -1 ? $('.bd_tit').eq(titleIndex).text().trim() : '';
-    const reading = $('.board_layout').eq(titleIndex).text().trim();
-    const scrapRes = {
-      titleIndex,
-      title,
-      reading
+    let reading = $('.board_layout').eq(titleIndex).text().trim();
+    reading = this.removeAngleBracketContent(reading);
+    const unwantedString = '주님의 말씀입니다.◎ 하느님, 감사합니다.';
+    if (reading.includes(unwantedString)) {
+      reading = reading.replace(unwantedString, '').trim();
     }
-    return scrapRes;
+
+    const readingRes: Reading = {
+      type: 'SCRIPTURE',
+      index: titleIndex,
+      title,
+      contents: reading
+    }
+    return readingRes;
+  }
+
+  async scrapGosphel($: cheerio.CheerioAPI): Promise<Reading> {
+    let titleIndex = -1;
+    $('.bd_tit').each((i, el) => {
+      if ($(el).text().trim() === '복음') {
+        titleIndex = i;
+        return false; // Break the loop
+      }
+    });
+    const title = titleIndex !== -1 ? $('.bd_tit').eq(titleIndex).text().trim() : '';
+    let reading = $('.board_layout').eq(titleIndex).text().trim();
+    reading = this.removeAngleBracketContent(reading);
+    const unwantedString = '주님의 말씀입니다.◎ 그리스도님, 찬미합니다.';
+    if (reading.includes(unwantedString)) {
+      reading = reading.replace(unwantedString, '').trim();
+    }
+    const readingRes: Reading = {
+      type: 'GOSPEL',
+      index: -1,
+      title,
+      contents: reading
+    }
+    return readingRes;
   }
 
   // date ex : '2024-12-25'
-  async scrapDay(date: string): Promise<ScrapRes[]> {
+  async scrapDay(date: string): Promise<Reading[]> {
     const url = `https://maria.catholic.or.kr/mi_pr/missa/missa.asp?menu=missa&gomonth=${date}`;
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
 
-    const scrapResList: ScrapRes[] = [];
+    const readingList: Reading[] = [];
     for (let i = 1; i <= 10; i++) {
-      const scrapRes = await this.scrapScriptures($, i);
-      if (scrapRes.titleIndex === -1) {
+      const readingRes = await this.scrapScriptures($, i);
+      if (readingRes.index === -1) {
         break;
       }
-      scrapResList.push(scrapRes);
+      readingList.push(readingRes);
     }
-    return scrapResList;
+    const scrapGosphelRes = await this.scrapGosphel($);
+    readingList.push(scrapGosphelRes)
+    return readingList;
+  }
+
+  removeAngleBracketContent(text: string): string {
+    return text.replace(/<[^>]*>/g, '').trim();
   }
 }
 
+// Reading type으로 변환해야할것
 type ScrapRes = {
   titleIndex: number,
   title: string,
   reading: string,
 }
+
--- src/types.ts
+++ src/types.ts
@@ -9,11 +9,8 @@ export type DailyDataStatus = 'DRAFT' | 'PUBLISHED';
 export type Reading = {
   type: ReadingType; // Scripture(독서) | Gospel(복음)
   index: number; // 제 1독서, 제 2독서 의 숫자
-  book: string; // 요나 예언서의 말씀에서의 요나서와 같은 책 이름
-  chapter: number; // 장
-  startVerse: number; // 시작 절
-  endVerse: number; // 끝 절
-  verses: string[]; // 절 마다 끊어서 배열로 저장
+  title: string; // 제목 (제 1독서, 제 2독서)
+  contents: string; // 내용
 };
 
 export type ReadingType = 'SCRIPTURE' | 'GOSPEL'; // 독서 | 말씀
